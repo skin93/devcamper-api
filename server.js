0d1fc5c623e434f48c9a67bdbf04fe68a9e62810
skin93 <arro.ciesielski@gmail.com>
2020-01-01 18:58:24 +0100

    Bootcamp model added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
@@ -29,13 +30,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow
+      .bold
   );
 });
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server & exit process
   server.close(() => process.exit(1));
 });
