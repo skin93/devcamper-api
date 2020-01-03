3d2fb1351d6d45b216da7877103e3b47d7a0cd86
skin93 <arro.ciesielski@gmail.com>
2020-01-03 19:32:15 +0100

    slugify added

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -98,6 +99,13 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  console.log('Slugify ran', this.name);
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
