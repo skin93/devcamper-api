bf68ee817f4574181159c68bae068e9eb6464b06
skin93 <arro.ciesielski@gmail.com>
2020-01-03 18:27:14 +0100

    errorHandler added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const errorHandler = require('./middleware/error.js');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
@@ -28,6 +29,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mout routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
