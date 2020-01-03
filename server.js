12e3ce0680da2d38e95f77a667cb8787b97bc6c7
skin93 <arro.ciesielski@gmail.com>
2020-01-03 18:15:57 +0100

    CRUD controllers added

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
