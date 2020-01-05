3962e16a39f950eca20f9313e5f1e4e438c12915
skin93 <arro.ciesielski@gmail.com>
2020-01-05 20:03:50 +0100

    course routes & controller added

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 
 const app = express();
 
@@ -29,6 +30,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mout routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
