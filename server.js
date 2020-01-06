24099d5c98ed88423389eb9732b443f30963999c
skin93 <arro.ciesielski@gmail.com>
2020-01-06 19:08:15 +0100

    registering user and encryptinc passwords added

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ dotenv.config({ path: './config/config.env' });
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -38,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mout routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
