7d53852e0b55f1a02aa27fea693b7d5a672c2ecc
skin93 <arro.ciesielski@gmail.com>
2020-01-07 16:01:44 +0100

    API security added

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,12 +11,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
+    // set token from cookie
   }
-
-  //   else if (req.cookies.token){
-  //       token = req.cookies.token
-  //   }
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
 
   // make sure token exists
   if (!token) {
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error.js');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
@@ -36,6 +42,29 @@ if (process.env.NODE_ENV === 'development') {
 
 // File uploading
 app.use(fileupload());
+
+// sanitize data
+app.use(mongoSanitize());
+
+// set security headers
+app.use(helmet());
+
+// prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100
+});
+
+app.use(limiter);
+
+// prevent http param pollution
+app.use(hpp());
+
+// enable CORS
+app.use(cors());
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
